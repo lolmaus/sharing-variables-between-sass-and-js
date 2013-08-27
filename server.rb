2926f19a2bd9aee1b7bbee5574cf5178d3b06af8
Andrey 'lolmaus' Mikhaylov <lolmaus@gmail.com>
2013-08-27 14:09:00 +0400

    Modified the readme for server.rb (in comments)

--- server.rb
+++ server.rb
@@ -1,18 +1,11 @@
 # Howto:
 # 1. Install Ruby if you ain't got one.
-# 2. Run `gem install sinatra`.
-# 3. Put this file into the project folder, next to your `index.html`.
-# 4. Run `ruby server.rb`.
-# 5. Open http://localhost:4567/ .
+# 2. Run `gem install bundler`.
+# 3. From your project root, run `bundle install`.
+# 4. Run `bundle exec ruby server.rb`.
+# 5. Open http://localhost:4567/ in your browser.
 
 require 'sinatra'
-
-configure do
-  mime_type :mp4, 'video/mp4'
-  mime_type :ogg, 'video/ogv'
-  mime_type :webm, 'video/webm'
-  set :public_folder, File.dirname(__FILE__)
-end
 
 get '/' do
   send_file File.join(settings.public_folder, 'index.html')
